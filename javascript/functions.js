39ab9f9c315c9d198b5e9a82252d64601ee56082
HoopaV <whizkidhv@gmail.com>
2019-06-17 19:46:20 -0500

    renamed dates.js to be more general added more html examples removed text size vw in styles.css
    Signed-off-by: HoopaV <whizkidhv@gmail.com>

--- javascript/functions.js
+++ javascript/functions.js
@@ -74,3 +74,19 @@ function drawImg()
     var img = document.getElementById("scream");
     ctx.drawImage(img, 10, 10);
 }
+
+
+function getLocation() {
+   var x = document.getElementById("demo")
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+    var x = document.getElementById("demo")
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
